b1f4e9795bd56a1aedb998baaaa311ca44d92de5
lol-chang <doclecsj@naver.com>
2023-07-19 21:20:32 +0900

    user data 추가
    mbti etc..

--- src/main/java/com/team/TeamProject/dto/MemberDto.java
+++ src/main/java/com/team/TeamProject/dto/MemberDto.java
@@ -20,6 +20,17 @@ public class MemberDto {
 
     private String password;
 
+    //user 매칭 변수
+    private String mbti;//유저 MBTI
+    private String role_fst;//맡고 싶은 역할 1순위
+    private String role_scd;//맡고 싶은 역할 2순위
+
+    private String project_cnt;//프로젝트 경험 횟수 int
+    private String leader_cnt;//팀장 경험 int
+    private String present_grade;//현재 학점 double
+    private String target_grade;//목표 학점 double
+
+
 
     //entity -> dto로 변경 필요
 
@@ -29,6 +40,13 @@ public class MemberDto {
         memberDto.setName(memberEntity.getName());
         memberDto.setEmail(memberEntity.getEmail());
         memberDto.setPassword(memberEntity.getPassword());
+        memberDto.setMbti(memberEntity.getMbti());
+        memberDto.setRole_fst(memberEntity.getRole_fst());
+        memberDto.setRole_scd(memberEntity.getRole_scd());
+        memberDto.setProject_cnt(memberEntity.getProject_cnt());
+        memberDto.setLeader_cnt(memberEntity.getLeader_cnt());
+        memberDto.setPresent_grade(memberEntity.getPresent_grade());
+        memberDto.setTarget_grade(memberEntity.getTarget_grade());
         return memberDto;
     }
 
--- src/main/java/com/team/TeamProject/entity/MemberEntity.java
+++ src/main/java/com/team/TeamProject/entity/MemberEntity.java
@@ -23,12 +23,27 @@ public class MemberEntity {
     private String email;
 
     private String password;
+
+    private String mbti;//유저 MBTI
+    private String role_fst;//맡고 싶은 역할 1순위
+    private String role_scd;//맡고 싶은 역할 2순위
+
+    private String project_cnt;//프로젝트 경험 횟수 int
+    private String leader_cnt;//팀장 경험 int
+    private String present_grade;//현재 학점 double
+    private String target_grade;//목표 학점 double
     public static MemberEntity toMemberEntity(MemberDto memberDto){
         MemberEntity memberEntity = new MemberEntity();
         memberEntity.setEmail(memberDto.getEmail());
         memberEntity.setPassword(memberDto.getPassword());
         memberEntity.setName(memberDto.getName());
-
+        memberEntity.setMbti(memberDto.getMbti());
+        memberEntity.setRole_fst(memberDto.getRole_fst());
+        memberEntity.setRole_scd(memberDto.getRole_scd());
+        memberEntity.setProject_cnt(memberDto.getProject_cnt());
+        memberEntity.setLeader_cnt(memberDto.getLeader_cnt());
+        memberEntity.setPresent_grade(memberDto.getPresent_grade());
+        memberEntity.setTarget_grade(memberDto.getTarget_grade());
         return memberEntity;
     }
 }
